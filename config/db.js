9b4424763d7d1da3112d82673a6251b057413a3d
nostick-games <mbazoge@me.com>
2025-01-21 18:25:32 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   console.log(process.env.MONGO_URI);
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      // useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
